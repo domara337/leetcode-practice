577f6fc7399eae38f3a3600c876ec7335d3b452c
domara <domara.best@gmail.com>
2025-07-31 16:21:58 +0300

    solved valid anagram leetcode

---
+++ valid_anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    // Create a character frequency map
+    const count = {};
+
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+        if (!count[char]) {
+            return false; // character not found or count is zero
+        }
+        count[char]--;
+    }
+
+    return true;
+};
+
+console.log(isAnagram("anagram", "nagaram")); // true
+console.log(isAnagram("rat", "car"));         // false
